014057dc3589bfc56caa6686d3c3a55ea1536c69
kuanyu <starmap0312@gmail.com>
2017-03-16 17:44:16 +0800

    add more

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,11 @@ from django.conf.urls import url
 
 from . import views
 
+# Django supports more elegant URL patterns:
+# ex. /newsarchive/<year>/<month>/
+#     instead of:
+#     /ME2/Sites/dirmod.asp?sid=&type=gen&mod=Core+Pages&gid=A6CD4967199A42D9B65B1B
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
 ]
