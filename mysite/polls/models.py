c6bdfc645e5c112c1f046bc7b05d17ed4c08a787
kuanyu <starmap0312@gmail.com>
2017-03-17 17:27:23 +0800

    add more

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -21,7 +21,8 @@ class Question(models.Model):
 
     # define our own behavior for a row object
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     # default: <Question: Question object>, so we can define more useful representation of a row 
     def __str__(self):
--- mysite/polls_generic/views.py
+++ mysite/polls_generic/views.py
@@ -1,3 +1,7 @@
+# Generic views
+# it abstracts the concepts of:
+# 1) ListView  : it abstracts the concepts of display a list of objects
+# 2) DetailView: it abstracts the concepts of display a detail page for a particular type of object
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
@@ -8,14 +12,20 @@ from polls.models import Choice, Question
 class IndexView(generic.ListView):
     template_name = 'polls_generic/index.html'
     context_object_name = 'latest_question_list'
+    # provide the context_object_name attribute, specifying latest_question_list
+    #   otherwise, the default is a automatically generated context variable: question_list
 
     def get_queryset(self):
         """Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'polls_generic/detail.html'
+    model = Question                            # each generic view needs to know what model it will be acting upon
+    template_name = 'polls_generic/detail.html' # specify the template name
+    # note:
+    #   it expects the primary key value captured from the URL to be called "pk"
+    #   so we've changed question_id to pk for the generic views
+    # the question variable is provided automatically, since we’re using a Django model (Question)
 
 class ResultsView(generic.DetailView):
     model = Question
