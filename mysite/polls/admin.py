435e2ab0fc36374ae5ec34d1c0071a5ae3e3f48e
kuanyu <starmap0312@gmail.com>
2017-03-20 13:01:34 +0800

    add more

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,35 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
 # Register your models here.
-admin.site.register(Question)     # need to add this to make the poll app modifiable in the admin
+# 1) add a line to make the poll app modifiable in the admin 
+#admin.site.register(Question)
+# then Django was able to construct a default form representation
+# 2) customize how the admin form looks and works:
+#class QuestionAdmin(admin.ModelAdmin):
+#    fields = ['pub_date', 'question_text']
+#admin.site.register(Question, QuestionAdmin)
+
+# Adding related objects
+# a Question has multiple Choices, there are two ways to display related objects:
+# 3.1)
+#admin.site.register(Choice)
+# Django knows that it has a Qustion ForeignKey
+#   when add a Choice item, we see a <select> box to select a Question item to associate with
+#   but this is an inefficient way of adding Choice objects to the system
+# 3.2)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']        # this adds a Filter sidebar (i.e. filter the change list by pub_date field)
+    search_fields = ['question_text'] # this adds a search box at the top of the change list (i.e. LIKE query)
+
+admin.site.register(Question, QuestionAdmin)
+
